d4f0313e3c91957330aac109872c6d8b7b9b4976
AbigailBlanco-davinci <86854315+abiblanco@users.noreply.github.com>
2021-10-27 18:11:17 -0300

    Delete FiguraTridimensional.java

--- FiguraTridimensional.java
+++
@@ -1,7 +0,0 @@
-public abstract class FiguraTridimensional {
-    final int dimensiones=3;
-    //metodos abstractos
-    public abstract double calcularVolumen();
-    public abstract double calcularSuperficie();
-
-}
